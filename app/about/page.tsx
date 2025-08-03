e7e75dd07b8bd3219aa07226922004536d0ba6b3
fatma-zaki <fatmazaki712@gmail.com>
2025-08-03 16:43:15 +0300

    add wrapper susbense for the activities

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,7 @@
 'use client';
 
 import { Suspense } from 'react';
-import { Header } from '@/components/Header';
-import { Footer } from '@/components/Footer';
+
 import AboutContent from './AboutContent';
 
 export default function AboutPage() {
